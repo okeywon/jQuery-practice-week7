040a5125c06c405ec7d0560a87c022f443fdfedb
Courtney Azar <courtney.a.azar@gmail.com>
2022-05-11 15:47:42 -0500

    created onReady function and created h1 logger

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 console.log('JS');
+
+// DOM = Document Object Model
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log("Let's do this!");
+
+    let titleHeader = $('h1');
+    // let titleHeader = <h1></h1>
+    console.log('titleheader is:', titleHeader); //ALWAYS LOG THESE - helpful for debugging.
+    // console.log the h1 'titleheader'
+}
